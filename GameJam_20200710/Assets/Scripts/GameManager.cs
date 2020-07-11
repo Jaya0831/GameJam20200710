1b39a33dbeef7ad393373baa000b0f0a817439e9
Liujiayi <liujy20010831@icloud.com>
2020-07-11 09:52:18 +0800

    fea:2020.7.11
    - UI界面
    - UIController
    - GameManager

--- GameJam_20200710/Assets/Scripts/GameManager.cs
+++ GameJam_20200710/Assets/Scripts/GameManager.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// 实例化
+    /// </summary>
+    private static GameManager _intance;
+    public static GameManager gameManager
+    {
+        get
+        {
+            return _intance;
+        }
+    }
+
+    /// <summary>
+    /// 关卡总数
+    /// </summary>
+    public static int levelNum = 3;
+    /// <summary>
+    /// 当前关卡
+    /// </summary>
+    public int level;
+    /// <summary>
+    /// 技能（总）
+    /// </summary>
+    public enum Skill
+    {
+        Walk,
+        Jump,
+        Push,
+        Pull
+    }
+    /// <summary>
+    /// 每一关对应可使用技能
+    /// </summary>
+    public List<Skill>[] skillForEachLevel = new List<Skill>[levelNum];
+    /// <summary>
+    /// 技能贴图
+    /// </summary>
+    public Sprite[] skillSprites;
+    /// <summary>
+    /// 每轮可选择的技能数量
+    /// </summary>
+    public int[] selectNums = new int[levelNum];
+
+    /// <summary>
+    /// 返回关卡对应可使用的技能
+    /// </summary>
+    /// <returns></returns>
+    public List<Skill> GetSkillsForThisLevel()
+    {
+        return skillForEachLevel[level];
+    }
+
+    public void LoadScence(int level)
+    {//todo:更新level？
+        SceneManager.LoadScene(level - 1);
+    }
+    private void Awake()
+    {
+        _intance = this;
+        selectNums[0] = 2;
+        List<Skill> level1 = new List<Skill> { Skill.Walk, Skill.Jump, Skill.Push, Skill.Pull };
+        skillForEachLevel[0] = level1;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- GameJam_20200710/Assets/Scripts/UIController.cs
+++ GameJam_20200710/Assets/Scripts/UIController.cs
@@ -1,18 +1,141 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button startButton;
+    public Button[] skillButtons;
+    public GameObject selectedButtonPrefabs;
+    public Button[] selectedButtons;
+    public GameObject selectedPanel;
+    /// <summary>
+    /// 不同SelectedButton数量时，每个按钮的位置
+    /// </summary>
+    private List<Vector2>[] positionOfSelectedButton = new List<Vector2>[GameManager.levelNum];
+    /// <summary>
+    /// 存储目前可以使用的技能
+    /// </summary>
+    private GameManager.Skill[] enableSkills;
+    private int[] matchedSkillButtons;
+    /// <summary>
+    /// 指向将要被决定的selectButton位置
+    /// </summary>
+    private int selectButtonPoint;
+    private bool[] isSelectButtonEmpty;
+
+    private void Awake()
     {
-        
+        List<Vector2> temp1 = new List<Vector2> { new Vector2(0, 0) };
+        //todo
+    }
+    private void Start()
+    {
+        ResetUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetUI()
     {
+        selectButtonPoint = 0;
+        isSelectButtonEmpty = new bool[selectedButtons.Length];
+        for (int i = 0; i < isSelectButtonEmpty.Length; i++)
+        {
+            isSelectButtonEmpty[i] = true;
+        }
+        enableSkills = new GameManager.Skill[selectedButtons.Length];
+        matchedSkillButtons = new int[selectedButtons.Length];
+        for (int i = 0; i < matchedSkillButtons.Length; i++)
+        {
+            matchedSkillButtons[i] = -1;
+        }
+        List<GameManager.Skill> temp = GameManager.gameManager.GetSkillsForThisLevel();
+        int len = temp.Count;
+        for (int i = 0; i < len; i++)
+        {
+            skillButtons[i].image.sprite = GameManager.gameManager.skillSprites[(int)temp[i]];
+        }
+        // SelectButton初始化不可以点
+        for (int i = 0; i < selectedButtons.Length; i++)
+        {
+            selectedButtons[i].enabled = false;
+        }
+    }
+    public void Select(int num)
+    {
+        selectedButtons[selectButtonPoint].image.sprite = skillButtons[num].image.sprite;
+        Debug.Log(selectButtonPoint);
+        enableSkills[selectButtonPoint] = GameManager.gameManager.skillForEachLevel[GameManager.gameManager.level][num];
+        matchedSkillButtons[selectButtonPoint] = num;
+        isSelectButtonEmpty[selectButtonPoint] = false;
+        selectedButtons[selectButtonPoint].enabled = true;
+        NextPoint();
+        skillButtons[num].enabled = false;
         
+        //todo：传递给playercontroller
+    }
+    public void Unselect(int num)
+    {
+        selectedButtons[num].image.sprite = null;
+        isSelectButtonEmpty[num] = true;
+        NextPoint();
+        skillButtons[matchedSkillButtons[num]].enabled = true;
+        selectedButtons[num].enabled = false;
+        matchedSkillButtons[num] = -1;
+    }
+    private void Update()
+    {
+        if (selectButtonPoint == selectedButtons.Length)
+        {
+            for (int i = 0; i < skillButtons.Length; i++)
+            {
+                skillButtons[i].enabled = false;
+            }
+            startButton.enabled = true;
+        }
+        else
+        {
+            for (int i = 0; i < skillButtons.Length; i++)
+            {
+                skillButtons[i].enabled = true;
+            }
+            for (int i = 0; i < matchedSkillButtons.Length; i++)
+            {
+                if (matchedSkillButtons[i]!=-1)
+                {
+                    skillButtons[matchedSkillButtons[i]].enabled = false;
+                }
+            }
+            startButton.enabled = false;
+        }
+        //for (int i = 0; i < isSelectButtonEmpty.Length; i++)
+        //{
+        //    Debug.Log(i+":"+isSelectButtonEmpty[i]);
+        //}
+    }
+    public void Determine()
+    {
+        for (int i = 0; i < enableSkills.Length; i++)
+        {
+            Debug.Log(enableSkills[i]);
+        }
+        for (int i = 0; i < selectedButtons.Length; i++)
+        {
+            selectedButtons[i].enabled = false;
+        }
+        startButton.enabled = false;
+    }
+    private void NextPoint()
+    {
+        for (int i = 0; i < isSelectButtonEmpty.Length; i++)
+        {
+            if (isSelectButtonEmpty[i])
+            {
+                selectButtonPoint = i;
+                return;
+            }
+        }
+        selectButtonPoint = isSelectButtonEmpty.Length;
     }
 }
+
